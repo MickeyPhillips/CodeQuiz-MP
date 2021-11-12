3017fa2053f6e2183a1ddd370df720521c54b12e
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-12 13:38:17 -0600

    restructured the questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,56 +5,28 @@ var highscore = document.querySelector('#high-score');
 var questions = [
     {  
         question: "Commonly used data types DO NOT include: ",
-        answers: {
-            1: "strings",
-            2: "booleans",
-            3: "alerts",
-            4: "numbers"
-        },
-        correct: "3"
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
     },
     {
         question: "The condition in an if / else statement is enclosed with ________. ",
-        answers: {
-            1: "quotes",
-            2: "curly brackets",
-            3: "parenthesis",
-            4: "square brackets"
-        },
-        correct: "3"
-
+        choices: ["quotes",  "curly brackets", "parenthesis", "square brackets"],
+        correct: "parenthesis"
     },
     {
         question: "Arrays in JavaScript can be used to store ________. ",
-        answers: {
-            1: "numbers and strings",
-            2: "other arrays",
-            3: "booleans",
-            4: "all of the above"
-        },
-        correct: "4"
-
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above"
     },
     {
         question: "String values must be enclosed within ________ when being assigned to variables. ",
-        answers: {
-            1: "commas",
-            2: "curly brackets",
-            3: "quotes",
-            4: "parenthesis"
-        },
-        correct: "3"
-
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correct: "quotes"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-        answers: {
-            1: "JavaScript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log"
-        },
-        correct: "4"
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correct: "console.log"
 
     }
 ];
